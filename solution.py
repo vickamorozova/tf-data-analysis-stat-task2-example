995f3876549ec3a17dc8cfe375dc468b28dd92ea
Viktoriya Morozova <117348526+vickamorozova@users.noreply.github.com>
2023-04-07 10:03:58 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,15 +6,10 @@ from scipy.stats import norm
 
 chat_id = 357282961 # Ваш chat ID, не меняйте название переменной
 
-def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    n = len(x)
-    alpha = 1 - p
-    s = np.sum(x) / 44
-    var = np.sum((x / 44 - s) ** 2) / (n - 1)
-    t = norm.ppf(1 - alpha / 2)
-    left = s - t * np.sqrt(var / n) * np.exp(-1 / 2) / np.sqrt(2 * (n - 1))
-    right = s + t * np.sqrt(var / n) * np.exp(-1 / 2) / np.sqrt(2 * (n - 1))
-    return left, right
+def solution(p: float, x: np.array) -> tuple: 
+    alpha = 1 - p 
+    n = len(x) 
+    left = (-min(-x) - 1 / 2) / (44**2 / 2) 
+    right = (-np.log(alpha) / n -min(-x) - 1 / 2) / (44**2 / 2) 
+    return left, \ 
+           right
